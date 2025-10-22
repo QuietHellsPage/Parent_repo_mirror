52d9ab7af2346c6fa2d5b29ebec3c529bd26322d
QuietHellsPage <baazhinga@yandex.ru>
2025-10-22 16:26:41 +0300

    Final comment checks

--- lab_1_keywords_tfidf/start.py
+++ lab_1_keywords_tfidf/start.py
@@ -47,7 +47,7 @@ def main() -> None:
     key_words_chi = get_top_n(chi_values, 10) or []
     print(', '.join(key_words_chi))
     result = key_words_chi
-    assert result, "Keywords are not extract"
+    assert result, "Keywords are not"
 
 if __name__ == "__main__":
     main()
