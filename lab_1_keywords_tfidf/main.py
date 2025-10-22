38e7f5a568ab04a0b25d25584142f29d0372e7f2
QuietHellsPage <baazhinga@yandex.ru>
2025-10-22 16:59:56 +0300

    merge test2

--- lab_1_keywords_tfidf/main.py
+++ lab_1_keywords_tfidf/main.py
@@ -172,8 +172,8 @@ def calculate_tf(frequencies: dict[str, int]) -> dict[str, float] | None:
     """
     if not check_dict(frequencies, str, int, False):
         return None
-    dict_len = sum(frequencies.values())
-    return {token: word_count / dict_len for token, word_count in frequencies.items()}
+    dict_le = sum(frequencies.values())
+    return {token: word_count / dict_le for token, word_count in frequencies.items()}
 
 
 
