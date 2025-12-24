aec42f6399211ec25ad619eed9e8a210a489a663
QuietHellsPage <baazhinga@yandex.ru>
2025-12-24 11:40:42 +0300

    proper constant

--- config/constants.py
+++ config/constants.py
@@ -8,6 +8,5 @@ PROJECT_ROOT = Path(__file__).parent.parent
 PROJECT_CONFIG_PATH = PROJECT_ROOT / "project_config.json"
 CONFIG_PACKAGE_PATH = PROJECT_ROOT / "config"
 CORE_UTILS_PACKAGE_PATH = PROJECT_ROOT / "core_utils"
-TRACKED_JSON_PATH_OBJ = str(PROJECT_ROOT/ "autosync" / "test_files.json")
-# TRACKED_JSON_PATH = str(TRACKED_JSON_PATH_OBJ.relative_to(PROJECT_ROOT))
+TRACKED_JSON_PATH_OBJ = str((CONFIG_PACKAGE_PATH / "external_pr_files" / "tracked_files.json").relative_to(PROJECT_ROOT))
 FLOAT_TOLERANCE = 4
