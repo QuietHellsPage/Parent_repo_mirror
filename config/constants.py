5fd2ae59da4c9f81c22cd6d948d21e2937d3a2d8
QuietHellsPage <baazhinga@yandex.ru>
2026-01-12 07:09:54 +0300

    code style

--- config/constants.py
+++ config/constants.py
@@ -8,5 +8,8 @@ PROJECT_ROOT = Path(__file__).parent.parent
 PROJECT_CONFIG_PATH = PROJECT_ROOT / "project_config.json"
 CONFIG_PACKAGE_PATH = PROJECT_ROOT / "config"
 CORE_UTILS_PACKAGE_PATH = PROJECT_ROOT / "core_utils"
-TRACKED_JSON_PATH = str((PROJECT_ROOT / "admin_utils" / "external_pr_files" / "test_files.json").relative_to(PROJECT_ROOT))
+TRACKED_JSON_PATH = str((PROJECT_ROOT /
+                         "admin_utils" /
+                         "external_pr_files" /
+                         "test_files.json").relative_to(PROJECT_ROOT))
 FLOAT_TOLERANCE = 4
